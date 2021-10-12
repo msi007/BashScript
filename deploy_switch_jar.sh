ac97db8a8d4f20f2fa050a3fb616743da69483da
Md. Saiful Islam <saifcse05@gmail.com>
2021-10-12 11:09:13 +0600

    rsrs aadded

--- deploy_switch_jar.sh
+++ deploy_switch_jar.sh
@@ -20,55 +20,50 @@ current_time=$(date '+%Y-%m-%d_%H:%M:%S')
 			BRed='\033[1;31m'         # Red
 			BGreen='\033[1;32m'       # Green
 
-
-
-function signaling_statuscheck(){
-signaling_staus=$(cat $signaling_loc/iTelSwitchPlusSignaling.log  | grep -w "started successfully")
-	if [ ! -z "{$signaling_staus}" ]; then
-	    echo "### ${BGreen} Switch Signaling started successfully ..\033[1;31m";
-	else 
-		echo "### ${BRed} Faled to start switch ..\033[1;31m";
-		
-	fi
-}
-
-
-function signaling_start(){
-		cd $signaling_loc
-		service  iTelSwitchPlusSignalingwintersoldier705 start > /dev/null 2>&1
-		 sleep 60
-signaling_statuscheck;		 
-}
-
-
 function jar_change(){
 	echo -e "## ${Green} Signaling jar upadting.........\\033[0;32m#"
 	cd $signaling_loc
 	mv iTelSwitchPlusSignaling.jar iTelSwitchPlusSignaling.jar_$current_time
 	mv $jar_location/iTelSwitchPlusSignaling.jar .
 	echo -e "## ${Green} Signaling jar updated susccessfully   \033[0;32m";
-signaling_start;
+signaling_start
 }
 
+function signaling_start(){
+		cd $signaling_loc
+		`sh runiTelSwitchPlusSignaling.sh > /dev/null 2>&1`
+		 sleep 60
+signaling_statuscheck		 
+}
+
+function signaling_statuscheck(){
+signaling_staus=$(cat $signaling_loc/iTelSwitchPlusSignaling.log  | grep -w "started successfully")
+	if [ ! -z "{$signaling_staus}" ]; then
+	    echo "### ${BGreen} Switch Signaling started successfully ..\033[1;31m";
+	else 
+		echo "### ${BRed} Faled to start switch ..\033[1;31m"
+		
+	fi
+}
 
 function stop_switch(){
 		echo -e " ## ${Red}Switch Signaling going to stop...   \033[0;32m#";
 		cd $signaling_loc
-		sh shutdowniTelSwitchPlusSignaling.sh > /dev/null 2>&1
+		`sh shutdowniTelSwitchPlusSignaling.sh > /dev/null 2>&1`
 		 sleep 1
-		sh shutdowniTelSwitchPlusSignaling.sh > /dev/null 2>&1
+		`sh shutdowniTelSwitchPlusSignaling.sh > /dev/null 2>&1`
 		 sleep 1
-		sh shutdowniTelSwitchPlusSignaling.sh > /dev/null 2>&1
+		`sh shutdowniTelSwitchPlusSignaling.sh > /dev/null 2>&1`
 		 sleep 1
-		rm -rf iTelSwitchPlusSignaling.log
+		`rm -rf iTelSwitchPlusSignaling.log`
 		 switch_procces_id=$(/bin/ps -fu $USER| grep "iTelSwitchPlusSignaling.jar wintersoldier705" | grep -v "grep" | gawk  '{print $2}')
 		 if [ -z $switch_procces_id ];then
 		 echo "Switch stopped Succesfully....."
 		     if [ ! -z $switch_procces_id ];then
-		    kill -9 $process_id > /dev/null 2>&1
-			 echo -e " ## {Red} Switch stopped Succesfully.....\\033[0;32m#";
+		    `kill -9 $process_id > /dev/null/ 2>&1`
+			 echo " ## {Red} Switch stopped Succesfully.....\\033[0;32m#";
 		     fi
 		 fi
 jar_change
 }
-stop_switch;
+stop_switch
